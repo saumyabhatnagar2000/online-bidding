b77fa39cd293161e89a9b7c420ed79265af79b37
credShivam <kumar.shivam@credgenics.com>
2022-12-22 15:58:48 +0530

    done added files for client

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authorization").replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
     const verify = jwt.verify(token, "cg-hackathon");
 
     const user = await User.findOne({ _id: verify._id, "tokens.token": token });
