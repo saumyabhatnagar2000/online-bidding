db5df8186dfcbebd96fc4fcf89f0f8439929c93a
credShivam <kumar.shivam@credgenics.com>
2022-12-22 22:21:06 +0530

    added listing routing

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const itemRouter = require("./routers/items");
+const listRouter = require("./routers/listing");
 const cors = require("cors");
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use(itemRouter);
+app.use(listRouter);
 
 app.listen(port, () => {
   console.log(`Up and running on localhost://${port}`);
