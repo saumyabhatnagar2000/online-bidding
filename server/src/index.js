506422c66849b04407888a3c813827d9960b24a7
Aman Srivastava <aman.srivastava@credgenics.com>
2022-12-23 02:30:35 +0530

    mail route added

--- server/src/index.js
+++ server/src/index.js
@@ -4,6 +4,8 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 const itemRouter = require("./routers/items");
 const listRouter = require("./routers/listing");
+const mailRouter = require("./routers/mail");
+
 const cors = require("cors");
 
 const app = express();
@@ -16,6 +18,7 @@ app.use(userRouter);
 app.use(taskRouter);
 app.use(itemRouter);
 app.use(listRouter);
+app.use(mailRouter)
 
 app.listen(port, () => {
   console.log(`Up and running on localhost://${port}`);
